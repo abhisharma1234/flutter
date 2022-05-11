01003ba65b7a7e80cda72d27310fd1365d9e6097
abhishek sharma <abhisharma.as45@yahoo.com>
2022-05-11 21:47:29 +0530

    commit on day 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/home_page.dart';
-
+import 'package:flutter_application/Login_page.dart';
+import 'package:flutter_application/utils/routes.dart';
+import 'Home_page.dart';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    // int data = 30;
-    // String nd = "jai mata di";
     return MaterialApp(
-      home:Homepage()
+      //  home:Homepage(),
+      themeMode: ThemeMode.light,
+      theme:
+          ThemeData(primarySwatch: Colors.deepPurple,
+           ),
+           //debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/login",
+      routes: {
+        MyRoutes.homeRoute: (context) => Homepage(),
+        MyRoutes.loginRoute: (context) => Loginpage()
+      },
     );
   }
 }
